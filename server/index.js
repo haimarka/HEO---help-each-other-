ded4816eb5ced685dac191bfc408f1ed3ff72701
Asras Farada <asrasfarada@gmail.com>
2022-02-14 19:09:40 +0200

    edit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express() ,
+
+PORT = process.env.PORT || 5000
+
+
+app.get('/',(req,res)=>{
+    res.send("hello")
+    console.log('hello!!!');
+})
+
+
+
+app.listen( PORT ,()=>{
+    console.log(`server is loading ...... ${PORT}`);
+})
