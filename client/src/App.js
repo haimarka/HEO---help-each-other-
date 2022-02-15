14ea4ae3dfdd05aa7c77f942a2373154a92dc72c
mayanelias <eliasmayan@gmail.com>
2022-02-15 11:13:24 +0200

    button

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import axios from "axios";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Team Winners!!!1</h1>
+      <h1 onClick={async()=>{
+        await axios.get('/click');
+      }}>Hello Team Winners!!!1</h1>
     </div>
   );
 }
