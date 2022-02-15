c57b7323065de95a5ff24057efe82bab05374048
ruth <rutiayaso0@gmail.com>
2022-02-15 10:51:16 +0200

    adding test for connecting server to client with axios request

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-<h1>Hello Team Winners!!!1</h1>
+      <h1>Hello Team Winners!!!1</h1>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -8,9 +8,11 @@ PORT = process.env.PORT || 5000
 app.get('/',(req,res)=>{
     res.send("hello")
     console.log('hello!!');
+});
+
+app.get("/click", (req, res) => {
+    console.log("the button was clicked");
 })
-
-
 
 app.listen( PORT ,()=>{
     console.log(`server is loading ...... ${PORT}`);
