6de1ebde44c11486503d53925d4f402cb6b3e853
Asras Farada <asrasfarada@gmail.com>
2022-02-15 11:05:07 +0200

    "edit"

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
-import express from "express";
-
+import express from "express" 
+import cors from "cors"
+import bodyParser from "body-parser"
 const app = express() ,
 
-PORT = process.env.PORT || 5000
+PORT = process.env.PORT || 5000;
+
+app.use(cors({origin:'*'}))
+
+app.use(bodyParser.json())
+
 
 
 app.get('/',(req,res)=>{
