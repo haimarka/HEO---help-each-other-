17e9f5d41b35debf85ac8aa285015b616a72ffe8
haimarka <haimarka1@gmail.com>
2022-02-16 15:23:17 +0200

    pages commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 import "./App.css";
 import axios from "axios";
-// import {BrowserRouter, Switch, Link} from '';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import LogIn from './pages/login/LogIn';
+import Register from './pages/register/Register';
+import Search from './pages/search/Search';
+import Navigation from "./components/Navigation";
+
 function App() {
+  <BrowserRouter>
   return (
     <div className="App">
+      <Navigation/>
       <h1 onClick={async()=>{
         await axios.get('/click');
       }}>Hello Team Winners For Ever!!!1</h1>
-
+    <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/About' render={<About/>}/>
+        <Route exact path='/LogIn' render={<LogIn/>}/>
+        <Route exact path='/Register' render={<Register/>}/>
+        <Route exact path='/Search' render={<Search/>}/>
+    </Switch>
     </div>
   );
+  </BrowserRouter>
 }
 
 export default App;
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function 
 () {
   return (
     <div>
         <h2>Navigation</h2>
+        <Link to='/'>Home</Link>
+        <Link to='/About'>About</Link>
+        <Link to='/LogIn'>LogIn</Link>
+        <Link to='/Register'>Register</Link>
+        <Link to='/Search'>Search</Link>
     </div>
   )
 }
