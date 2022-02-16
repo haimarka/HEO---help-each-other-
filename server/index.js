6894956aa345d76cf2200f588838e6349b880135
Asras Farada <asrasfarada@gmail.com>
2022-02-16 21:02:07 +0200

    index.js

--- server/index.js
+++ server/index.js
@@ -8,25 +8,27 @@ const objectID = mongo.ObjectId;
 const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017";
 const DATA_BASE = "freeToHelp";
 const PORT = process.env.PORT || 5000;
-
-import { registerVolunteers ,clientRegister} from "./utils/register.js";
+import userRoutes from "./routes/user";
+import { registerVolunteers, clientRegister } from "./utils/user.js";
 import { searchByCity } from "./utils/search.js";
-import {creatData} from "./utils/data.js"
+import { creatData } from "./utils/data.js";
 
 const app = express();
 app.use(express.json());
 
 app.post("/", (req, res) => {
-  console.log('working post');
+  console.log("working post");
 });
 
-app.get("/api",(req,res)=>{
-  creatData(req,res)
-})
+app.use("/register", userRoutes);
 
-app.get("/volunteer/:city",(req,res)=>{
-  searchByCity(req,res)
-})
+app.get("/api", (req, res) => {
+  creatData(req, res);
+});
+
+app.get("/volunteer/:city", (req, res) => {
+  searchByCity(req, res);
+});
 
 app.post("/register/volunteer", (req, res) => {
   registerVolunteers(req, res);
@@ -35,7 +37,6 @@ app.post("/register/volunteer", (req, res) => {
 app.post("/register/client", (req, res) => {
   clientRegister(req, res);
 });
-
 
 app.listen(PORT, () => {
   console.log(`server is loading ...... ${PORT}`);
