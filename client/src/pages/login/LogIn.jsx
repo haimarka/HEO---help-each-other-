22bc3a7610472f653dd877d8c70312cba9e7bc05
EzraDasa <ezradasa93@gmail.com>
2022-02-17 12:09:08 +0200

    commit

--- client/src/pages/login/LogIn.jsx
+++ client/src/pages/login/LogIn.jsx
@@ -7,13 +7,20 @@ export default function LogIn() {
   function postLogin(e) {
     e.preventDefault()
     axios.post("/api/volunteers/login",{phoneNumber,password})
-    .then((res)=>{console.log(res);})
+    .then((res)=>{console.log(res.data);})
     .catch((err)=>{console.log(err);})
   }
   return (
     <form onSubmit={postLogin}>
       <label>phone:</label><br/>
-      <input type="text" placeholder='05455******' onChange={(e)=>setPhoneNumber(e.target.value)}/><br/>
+      <input type="text" placeholder='05455******' onBlur={(e)=>{
+        let number = e.target.value;
+        if(number[0] == "0"){
+          number = number.substring(1)
+        }
+        console.log(number);
+        setPhoneNumber("+972"+number)
+        }}/><br/>
       <label>password:</label><br/>
       <input type="password" placeholder='******' onChange={(e)=>setPassword(e.target.value)}/><br/>
       <button type='submit'>Login</button>
