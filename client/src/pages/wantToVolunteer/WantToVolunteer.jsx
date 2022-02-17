e67e143392bf23d85c062c94560b04e0066b0f72
Asras Farada <asrasfarada@gmail.com>
2022-02-17 13:06:06 +0200

    commit

--- client/src/pages/wantToVolunteer/WantToVolunteer.jsx
+++ client/src/pages/wantToVolunteer/WantToVolunteer.jsx
@@ -5,11 +5,7 @@ import { Spinner } from "react-bootstrap";
 import PhoneNumber from "../authPhone/PhoneNumber";
 import './register.css'
 // import "bootstrap/dist/css/bootstrap.min.css";
-<<<<<<< HEAD:client/src/pages/wantToVolunteer/WantToVolunteer.jsx
-import style from "./wantToVolunteer.module.css";
-=======
-// import style from "./register.module.css";
->>>>>>> 22bc3a7610472f653dd877d8c70312cba9e7bc05:client/src/pages/register/Register.jsx
+import style from "./wantToVolunteer.css";
 import { async, isIndexedDBAvailable } from "@firebase/util";
 const Register = ({auth , setAuth}) => {
   const [data, setData] = useState([]);
@@ -89,191 +85,6 @@ const Register = ({auth , setAuth}) => {
 
 
   return (
-<<<<<<< HEAD:client/src/pages/wantToVolunteer/WantToVolunteer.jsx
-
-    <div >
-      <div >
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (password){
-              registerForm();}
-          }}
-        >
-         <input
-      type="file"
-      accept="image/*"
-      onChange={fileSelectHandler}
-    />
-    <br />
-    <button onClick={() => fileUpload()}>upload</button>
-   
-          <p >FreeToHelp</p>
-          <div >
-            <label>FULL-NAME</label>
-            <br />
-            <input
-              
-              type="text"
-              placeholder="Full-Name"
-              required
-              onBlur={(e) => {
-                setFullName(e.target.value);
-              }}
-            />
-            <br />
-            <label>EMAIL</label>
-            <br />
-            <input
-              
-              type="email"
-              placeholder="example@test.com"
-              onBlur={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-            <br />
-            <label>CITY</label>
-            <br />
-            {data.cities?.length ? (
-              <select onBlur={(e)=>{setCity(e.target.value);}}>
-                {data.cities.map((item, i) => {
-                  return (
-                    <option
-                      value={item.english_name}
-                      key={i}
-                    >
-                      {item.english_name}
-                    </option>
-                  );
-                })}
-              </select>
-            ) : (
-              ""
-            )}
-            <br />
-            <label>CATEGORIES</label>
-            <br />
-            {data.categories?.length ? (
-              <div>
-                {data.categories.map((item, i) => {
-                  return (
-                    <label key={i}>
-                      {item.english_name}
-                      <input
-                        type="checkbox"
-                        key={i}
-                        onClick={(e) => {
-                          const arrayCategories=[...categories];
-                          console.log(e.target.checked);
-                          if(e.target.checked == true){
-                            arrayCategories.push(item.english_name)
-                            setCategories(arrayCategories)
-                          }else{
-                            let check = arrayCategories.findIndex((element)=>element == item.english_name);
-                            arrayCategories.splice(check,1)
-                            setCategories(arrayCategories)
-                          }
-                       
-                        }}
-                      />
-                    </label>
-                  );
-                })}
-              </div>
-            ) : (
-              ""
-            )}
-            <br />
-            <label>OCCUPATIONS</label>
-            <br />
-            {data.occupation?.length ? (
-              <div>
-                {data.occupation.map((item, i) => {
-                  return (
-                    <label key={i}>
-                      {item.english_name}
-                      <input
-                        type="checkbox"
-                        key={i}
-                        onChange={(e) => {
-                          setOccupations(e.target.value);
-                        }}
-                        onClick={(e) => {
-                          const arrayOccupations=[...occupations];
-                          console.log(e.target.checked);
-                          if(e.target.checked == true){
-                            arrayOccupations.push(item.english_name)
-                            setOccupations(arrayOccupations)
-                          }else{
-                            let check = arrayOccupations.findIndex((element)=>element == item.english_name);
-                            arrayOccupations.splice(check,1)
-                            setOccupations(arrayOccupations)
-                          }
-                       
-                        }}
-                      />
-                    </label>
-                  );
-                })}
-              </div>
-            ) : (
-              ""
-            )}
-            <br />
-            <label>GENDER</label>
-            <br />
-            <select onChange={(e) => {
-                  setGender(e.target.value);
-                }}>
-              <option
-                value="Male"
-              >
-                MEN
-              </option>
-              <option
-                value="Female"
-              >
-                WOMEN
-              </option>
-            </select>
-            <br />
-            <label>AVAILABILITY</label>
-            <br />
-            <input
-              onChange={(e) => {
-                setStart(e.target.value);
-              }}
-              type="time"
-            />
-            <input
-              onChange={(e) => {
-                setEnd(e.target.value);
-              }}
-              type="time"
-            />
-            {
-            loading 
-            ?              
-              <Spinner animation="border" variant="danger" />
-            : 
-              ""
-            }
-          </div>
-        <PhoneNumber setTelephone={setTelephone} setPassword={setPassword} password={password}/>
-          {password ? 
-           <input
-           
-           autoComplete="on"
-           type="submit"
-           value="Register"
-           />
-           : 
-           ""} 
-           <p style={{ color: "red" }}>{errorMessage ? errorMessage : ""}</p>
-        </form>
-        <img src={fileLink}/>
-=======
     <div className="container h-100">
       <div className="d-flex justify-content-center h-100">
         <div className="user_card">
@@ -475,7 +286,6 @@ const Register = ({auth , setAuth}) => {
             </form>
             </div>
           </div>
->>>>>>> 22bc3a7610472f653dd877d8c70312cba9e7bc05:client/src/pages/register/Register.jsx
       </div>
     </div>
   );
